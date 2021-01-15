e13ff22521c657f4ec67e34cd9c575157951dd14
jennneth <johansson_jen@hotmail.com>
2021-01-15 11:20:53 -0800

    building routes in app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+#design a Flask API based on the queries that you have just developed.
+from flask import Flask, jsonify
+
+##########################
+#Flask Setup
+##########################
+#initialize Flask app
+app=Flask(__name__)
+
+#########################
+#Flask Routes
+#########################
+#Home page
+#List all routes available
+@app.route("/")
+def index():
+    return(
+        f"Welcome to the API"<br/>
+        f"Available routes:"<br/>
+        f"/api/v1.0/precipitation"<br/>
+        )
+
+@app.route("/api/v1.0/precipitation")
+
+  * Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
+
+  * Return the JSON representation of your dictionary.
+
+* `/api/v1.0/stations`
+
+  * Return a JSON list of stations from the dataset.
+
+* `/api/v1.0/tobs`
+  * Query the dates and temperature observations of the most active station for the last year of data.
+
+  * Return a JSON list of temperature observations (TOBS) for the previous year.
+
+* `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
+
+  * Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+  * When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
+
+  * When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
+
+
+
+
